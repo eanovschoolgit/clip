0d42cd6c8d7ff50ba3789707d3c6ef51bf76939c
eanovschoolgit <82503976+eanovschoolgit@users.noreply.github.com>
2021-04-18 15:46:45 +0200

    Update and rename Scripts/ScriptProcessors/eanovclip/Interface.js to Scripts/Processeurs de script/eanovclip/Interface.js

--- Scripts/Processeurs de script/eanovclip/Interface.js
+++ Scripts/Processeurs de script/eanovclip/Interface.js
@@ -14,15 +14,6 @@ const var Knob1 = Content.getComponent("Knob1");
 
 
 
-inline function onKnob1Control(component, value)
-{
-	ScriptFX1.setAttribute(ScriptFX1.peak, value);
-	Knob2.setValue(value);
-    Knob2.changed();
-    Knob3.setValue(value);
-    Knob3.changed();
-    
-};
 
 Content.getComponent("Knob1").setControlCallback(onKnob1Control);
 
